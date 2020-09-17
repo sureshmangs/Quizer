4a0a259a515b038cf83e83f56181cd057dde027e
sureshmangs <sureshmangs1@gmail.com>
2020-09-17 22:38:06 +0530

    :rocket: Connecting to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send("<h1>Server is super high</h1>")
+    res.send("Server is super high")
 })
+
+
+// connect to database
+try {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+    mongoose.connection.on('connected', () => {
+        console.log(`Connected to MongoDB successfully`)
+    })
+} catch (err) {
+    console.log('Error occured while connecting to DB')
+}
 
 
 const PORT = process.env.PORT || 5000;
