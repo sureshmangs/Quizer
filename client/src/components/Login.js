c8698129991e043dc07212b52ea376e5e5e02cbd
sureshmangs <sureshmangs1@gmail.com>
2020-09-23 18:55:15 +0530

    :coffee: Implementing Google Oauth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import GoogleLogin from 'react-google-login';
+import { CLIENT_ID } from '../config/keys'
+
+import * as actions from '../actions/index';
+
+
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.responseGoogle = this.responseGoogle.bind(this);
+    }
+
+    async responseGoogle(res) {
+        console.log('responseGoogle is ', res);
+        console.log('typeof res ', typeof res);
+        console.log('access token is ', res.accessToken)
+        console.log('google id', res.profileObj.googleId)
+
+        await this.props.oauthGoogle(res.accessToken);
+        if (!this.props.errorMessage) {
+            this.props.history.push('/');
+        }
+    }
+
     render() {
         return (
             <div className="container-fluid pt-5">
@@ -16,8 +40,17 @@ class Login extends Component {
                                         <div className="card-body">
                                             <h5 className="card-title text-center">Sign In</h5>
                                             <form className="form-signin">
-                                                <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fa fa-google mr-2"></i> Sign in with Google</button>
-                                                <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                                                <GoogleLogin
+                                                    clientId={CLIENT_ID}
+                                                    render={renderProps => (
+                                                        <button onClick={renderProps.onClick} disabled={renderProps.disabled} className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fa fa-google mr-2"></i> Sign in with Google</button>
+                                                    )}
+                                                    scope="openid profile email"
+                                                    onSuccess={this.responseGoogle}
+                                                    onFailure={this.responseGoogle}
+                                                    isSignedIn={true}
+                                                />
+                                                <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
                                             </form>
                                         </div>
                                     </div>
@@ -32,4 +65,12 @@ class Login extends Component {
 }
 
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        isAuth: state.auth.isAuth,
+        errorMessage: state.auth.errorMessage,
+        accessTokenG: state.auth.accessTokenG
+    }
+}
+
+export default connect(mapStateToProps, actions)(Login)
