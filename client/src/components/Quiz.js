2d6389b0b23410a57592248efc72888b5366f46b
sureshmangs <sureshmangs1@gmail.com>
2020-09-29 22:16:12 +0530

    :zap: Creating Quiz component

---
+++ client/src/components/Quiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Spinner from './Spinner';
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div>
+                <Spinner />
+            </div>
+        )
+    }
+}
+
+export default Quiz
