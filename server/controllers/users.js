02aa2a62709c6178482e9bc90566a727d457a701
sureshmangs <sureshmangs1@gmail.com>
2020-10-03 22:43:07 +0530

    :zap: signing token using jsonwebtoken

---
+++ server/controllers/users.js
@@ -0,0 +1,29 @@
+const JWT = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+signToken = user => {
+    return JWT.sign({
+        iss: 'Quizer',
+        sub: user.id,
+        iat: new Date().getTime(), // current time
+        exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day ahead
+    }, process.env.JWT_SECRET);
+}
+
+
+module.exports = {
+    googleOAuth: async (req, res, next) => {
+        // Generate token
+        console.log('In google oauth');
+        const token = signToken(req.user);
+        res.status(200).json({ token });
+    },
+
+    checkAuth: async (req, res, next) => {
+        console.log('We managed to get here!');
+        res.json({ success: true });
+    }
+}
